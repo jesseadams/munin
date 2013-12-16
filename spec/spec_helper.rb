797f6d1a31da300231722ef091728f458fd0d8af
Arnold Krille <a.krille@b-c-s.de>
2013-12-16 18:21:52 +0100

    Depend on chefspec 3.1.0.beta.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
-require 'berkshelf'
+#require 'berkshelf'
 require 'chefspec'
+require 'chefspec/deprecations'
+require 'chefspec/berkshelf'
 
-Berkshelf.ui.mute do
-  Berkshelf::Berksfile.from_file('Berksfile').install(path: 'vendor/cookbooks')
-end
+#Berkshelf.ui.mute do
+#  Berkshelf::Berksfile.from_file('Berksfile').install(path: 'vendor/cookbooks')
+#end
