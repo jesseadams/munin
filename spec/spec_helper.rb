23098b354c7fed8affc9b5c280a7dcebc95161da
Arnold Krille <a.krille@b-c-s.de>
2013-12-16 18:22:38 +0100

    port to chefspec3.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-#require 'berkshelf'
 require 'chefspec'
-require 'chefspec/deprecations'
 require 'chefspec/berkshelf'
 
-#Berkshelf.ui.mute do
-#  Berkshelf::Berksfile.from_file('Berksfile').install(path: 'vendor/cookbooks')
-#end
